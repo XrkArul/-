2e98f90d1e0f6a678e22c8dc1ddd2a3aae6a73fa
XrkArul <93807432+XrkArul@users.noreply.github.com>
2024-03-10 17:48:56 +0800

    Base

---
+++ EnemyManager.ts
@@ -0,0 +1,75 @@
+import EntityManager from './EntityManager';
+import { IEntity } from '../Levels';
+import EventManager from '../Runtime/EventManager';
+import { EVENT_ENUM } from '../Enum';
+import { DIRECTION_ENUM, ENTITY_STATE_ENUM } from '../Enum';
+import DataManager from '../Runtime/DataManager';
+
+/***
+ * 敌人基类，实现面朝人物和死亡
+ */
+export default abstract class EnemyManager extends EntityManager {
+  static componentName = 'EnemyManager'; // 设置组件的名字
+
+  init(params: IEntity) {
+    super.init(params);
+    EventManager.Instance.on(EVENT_ENUM.ATTACK_ENEMY, this.onDead, this);
+    EventManager.Instance.on(EVENT_ENUM.PLAYER_MOVE_END, this.onChangeDirection, this);
+  }
+
+  onDestroy() {
+    EventManager.Instance.off(EVENT_ENUM.ATTACK_ENEMY, this.onDead);
+    EventManager.Instance.off(EVENT_ENUM.PLAYER_MOVE_END, this.onChangeDirection);
+  }
+
+  start() {
+    this.onChangeDirection(true);
+  }
+
+  /***
+   * 根据玩家在敌人的象限改变敌人的朝向
+   */
+  onChangeDirection(init = false) {
+    if (this.state === ENTITY_STATE_ENUM.DEATH || !DataManager.Instance.player) {
+      return;
+    }
+    const { x: playerX, y: playerY } = DataManager.Instance.player;
+    const disX = Math.abs(playerX - this.x);
+    const disY = Math.abs(playerY - this.y);
+
+    //确保敌人在初始化的时候调整一次direction
+    if (disX === disY && !init) {
+      return;
+    }
+
+    //第一象限
+    if (playerX >= this.x && playerY <= this.y) {
+      this.direction = disX >= disY ? DIRECTION_ENUM.RIGHT : DIRECTION_ENUM.TOP;
+
+      //第二象限
+    } else if (playerX <= this.x && playerY <= this.y) {
+      this.direction = disX >= disY ? DIRECTION_ENUM.LEFT : DIRECTION_ENUM.TOP;
+
+      //第三象限
+    } else if (playerX <= this.x && playerY >= this.y) {
+      this.direction = disX >= disY ? DIRECTION_ENUM.LEFT : DIRECTION_ENUM.BOTTOM;
+
+      //第四象限
+    } else if (playerX >= this.x && playerY >= this.y) {
+      this.direction = disX >= disY ? DIRECTION_ENUM.RIGHT : DIRECTION_ENUM.BOTTOM;
+    }
+  }
+
+  /***
+   * 所有敌人都会接收到死亡事件，不过只有跟id跟自己相同的才会死亡
+   * @param id
+   */
+  onDead(id: string) {
+    if (this.state === ENTITY_STATE_ENUM.DEATH) {
+      return;
+    }
+    if (this.id === id) {
+      this.state = ENTITY_STATE_ENUM.DEATH;
+    }
+  }
+}
---
+++ EntityManager.ts
@@ -0,0 +1,57 @@
+import { Component } from '@eva/eva.js';
+import { DIRECTION_ENUM, DIRECTION_ORDER_ENUM, ENTITY_STATE_ENUM, PARAMS_NAME_ENUM, ENTITY_TYPE_ENUM } from '../Enum';
+import StateMachine from './StateMachine';
+import { randomByLength } from '../Utils';
+import { TILE_HEIGHT, TILE_WIDTH } from '../Scenes/Battle/GameObjects/Tile/Tile';
+import { IEntity } from '../Levels';
+
+export const ENTITY_WIDTH = 128;
+export const ENTITY_HEIGHT = 128;
+
+/***
+ * 实体类，实体必须具备方向和状态
+ */
+export default abstract class EntityManager extends Component {
+  static componentName = 'EntityManager'; // 设置组件的名字
+
+  id: string = randomByLength(12);
+  x: number; //坐标
+  y: number;
+  type: ENTITY_TYPE_ENUM;
+  private _state: ENTITY_STATE_ENUM;
+  private _direction: DIRECTION_ENUM;
+  fsm: StateMachine;
+
+  init(params: IEntity) {
+    this.x = params.x;
+    this.y = params.y;
+    this.state = params.state;
+    this.direction = params.direction;
+  }
+
+  /***
+   * 更新人物位置，把虚拟坐标（1,1）转为屏幕实际位置
+   */
+  update() {
+    this.gameObject.transform.position.x = this.x * TILE_WIDTH - TILE_WIDTH * 1.5;
+    this.gameObject.transform.position.y = this.y * TILE_HEIGHT - TILE_HEIGHT * 1.5;
+  }
+
+  get direction() {
+    return this._direction;
+  }
+
+  set direction(newDirection) {
+    this._direction = newDirection;
+    this.fsm.setParams(PARAMS_NAME_ENUM.DIRECTION, DIRECTION_ORDER_ENUM[this._direction]);
+  }
+
+  get state() {
+    return this._state;
+  }
+
+  set state(newState) {
+    this._state = newState;
+    this.fsm.setParams(newState, true);
+  }
+}
